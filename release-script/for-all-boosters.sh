97289ba90b9a47164333d4ecabfbc4ef53e4d22a
Chris Laprun <metacosm@gmail.com>
2018-02-19 12:50:47 +0100

    Better handle non-existing branch. Improve output.

--- release-script/for-all-boosters.sh
+++ release-script/for-all-boosters.sh
@@ -4,6 +4,7 @@ set -e
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 
 CURRENT_DIR=`pwd`
@@ -17,23 +18,33 @@ do
     if true ;
     then
         pushd $BOOSTER >/dev/null
-        for BRANCH in {redhat,master}
+
+        echo -e "${BLUE}> ${YELLOW}${BOOSTER}${BLUE}${NC}"
+
+        for BRANCH in redhat
         do
             # assumes "official" remote is named 'upstream'
-            git fetch upstream >/dev/null && git co $BRANCH >/dev/null && git rebase upstream/$BRANCH >/dev/null
+            git fetch upstream >/dev/null
 
-            # if we need to replace a multi-line match in the pom file of each booster, for example:
-            # perl -pi -e 'undef $/; s/<properties>\s*<\/properties>/replacement/' pom.xml
-
-            if [ -e "$1" ]; then
-                echo -e "${BLUE}Running ${YELLOW}${1}${BLUE} script on ${YELLOW}${BRANCH}${BLUE} branch of ${YELLOW}${BOOSTER}.${NC}"
-                source $1
+            # check if branch exists, otherwise skip booster
+            if ! git show-ref --verify --quiet refs/heads/${BRANCH}; then
+                echo -e "\t${GREEN}${BRANCH}${BLUE}: ${RED}Branch doesn't exist. Skipping.${NC}"
             else
-                echo -e "${BLUE}No script was provided or ${YELLOW}${1}${BLUE} doesn't exist in ${YELLOW}`pwd`${BLUE} directory.${NC}"
-                echo -e "${BLUE}Only refreshed local code.${NC}"
+                git co -q ${BRANCH} >/dev/null && git rebase upstream/${BRANCH} >/dev/null
+
+                # if we need to replace a multi-line match in the pom file of each booster, for example:
+                # perl -pi -e 'undef $/; s/<properties>\s*<\/properties>/replacement/' pom.xml
+
+                if [ -e "$1" ]; then
+                    echo -e "\t${GREEN}${BRANCH}${BLUE}: Running ${YELLOW}${1}${BLUE} script.${NC}"
+                    source $1
+                else
+                    echo -e "\t${GREEN}${BRANCH}${BLUE}: No script provided. Only refreshed code.${NC}"
+                fi
             fi
         done
+
+        echo -e "----------------------------------------------------------------------------------------\n"
         popd >/dev/null
-        echo -e "${BLUE}==> Processing ${YELLOW}${BRANCH}${BLUE} branch of ${YELLOW}${BOOSTER}${BLUE} finished.${NC}"
     fi
 done
