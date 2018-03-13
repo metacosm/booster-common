d4fd76cf2b125c4fa46ac9b1cd0a8a6f17010de8
Georgios Andrianakis <geoand@gmail.com>
2018-03-13 17:14:49 +0200

    Remove debug commands - change_version is now fully functional

--- release-script/for-all-boosters.sh
+++ release-script/for-all-boosters.sh
@@ -173,9 +173,6 @@ change_version() {
     
     if [ $? -eq 0 ]; then
         find . -name "*.versionsBackup" -delete
-        git diff
-        git reset --hard upstream/${BRANCH}
-        return 0
 
         # Only attempt committing if we have changes otherwise the script will exit
         if [[ `git status --porcelain` ]]; then
