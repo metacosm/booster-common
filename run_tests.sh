456ebc8d2bab0f8d83b9f464bbc13ff6c84a619d
Georgios Andrianakis <geoand@gmail.com>
2018-03-15 11:54:06 +0200

    Move execution of each test into specific function

--- run_tests.sh
+++ run_tests.sh
@@ -11,6 +11,25 @@ cd $(mktemp -d)
 declare -a boosters=( "http" "health-check" "crud" "configmap" "circuit-breaker" )
 declare -a failed=( )
 
+execute_test() {
+    local canonical_name=$1
+    local git_name=$2
+
+    cd ${git_name}
+
+    echo "Running tests of booster ${canonical_name} in ${PWD}"
+
+    mvn -q -B clean verify -Popenshift,openshift-it ${MAVEN_EXTRA_OPTS:-}
+    if [ $? -eq 0 ]; then
+        echo "Successfully tested ${canonical_name}"
+    else
+        echo "Tests of ${canonical_name} failed"
+        failed+=( ${canonical_name} )
+    fi
+
+    cd ../
+}
+
 for booster in "${boosters[@]}"
 do
     echo "Cloning booster ${booster}"
@@ -18,19 +37,8 @@ do
     booster_git_name=spring-boot-${booster}-booster
     git clone -q -b ${branch} https://github.com/snowdrop/${booster_git_name}
 
-    cd ${booster_git_name}
+    execute_test ${booster} ${booster_git_name}
 
-    echo "Running tests of booster ${booster}"
-
-    mvn -q -B clean verify -Popenshift,openshift-it ${MAVEN_EXTRA_OPTS:-}
-    if [ $? -eq 0 ]; then
-        echo "Successfully tested ${booster}"
-    else
-        echo "Tests of ${booster} failed"
-        failed+=( ${booster} )
-    fi
-
-    cd ../
 done
 
 echo "Done testing"
